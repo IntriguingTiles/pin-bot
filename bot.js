1a17ac01f7cafdbc0c1b8b27e57e311ad7b1f132
IntriguingTiles <jnickw@outlook.com>
2021-10-27 20:23:42 -0600

    Remove pin logging
    This was super silly and did not scale well at all.
    Restarts took ages since we were fetching pins for all channels.

--- bot.js
+++ bot.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 const Discord = require("discord.js");
-const Enamp = require("enmap");
+const Enmap = require("enmap");
 
-const client = new Discord.Client({ disableEveryone: true, partials: ["MESSAGE", "CHANNEL", "REACTION"] });
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS], allowedMentions: [], partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 
 const prefix = "+";
 
-const cachedPins = new Map();
-
-const guildSettings = new Enamp({
+const guildSettings = new Enmap({
     name: "guildSettings",
     fetchAll: false,
     autoFetch: true,
@@ -17,7 +15,6 @@ const guildSettings = new Enamp({
 
 const defaultSettings = {
     pinChannel: null,
-    logChannel: null,
     pinReactThreshold: 0, // 0 means this is turned off
     pins: [] // array of message ids that we've posted in the pin channel
 };
@@ -35,123 +32,51 @@ client.on("warn", console.warn);
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.username}`);
 
-    // cache pins
-    guildSettings.fetchEverything();
-    guildSettings.filter(gs => gs.logChannel || gs.pinChannel).forEach((gs, id) => {
-        if (!client.guilds.cache.has(id)) return;
-        client.guilds.cache.get(id).channels.cache.filter(ch => ch.type === "text" && ch.permissionsFor(client.user).has("VIEW_CHANNEL")).forEach(async ch => {
-            const pins = await ch.messages.fetchPinned();
-            cachedPins.set(ch.id, pins);
-            console.log(ch.name);
-        });
-    });
-    guildSettings.evict(client.guilds.cache.keyArray());
-
     client.user.setActivity("+help");
 });
 
 client.on("guildCreate", async guild => {
     guildSettings.set(guild.id, defaultSettings);
-    guild.channels.filter(ch => ch.type === "text" && ch.permissionsFor(client.user).has("VIEW_CHANNEL")).forEach(async ch => {
-        const pins = await ch.messages.fetchPinned();
-        cachedPins.set(ch.id, pins);
-        console.log(ch.name);
-    });
 });
 
 client.on("guildDelete", async guild => {
     guildSettings.delete(guild.id);
-    guild.channels.forEach(ch => cachedPins.delete(ch.id));
 });
 
-client.on("channelCreate", async ch => {
-    // channel created, fetch the pins and add it to our map
-    if (ch.type !== "text") return;
-    if (!ch.permissionsFor(client.user).has("VIEW_CHANNEL")) return;
-    if (!guildSettings.get(ch.guild.id)) return;
-    if (!guildSettings.get(ch.guild.id).logChannel && !guildSettings.get(ch.guild.id).pinChannel) return;
-    const pins = await ch.messages.fetchPinned();
-    cachedPins.set(ch.id, pins);
-});
-
-client.on("channelDelete", async ch => {
-    // channel deleted, remove it from our map
-    if (ch.type !== "text") return;
-    if (cachedPins.has(ch.id)) cachedPins.delete(ch.id);
-});
-
-client.on("channelPinsUpdate", async ch => {
-    // we need to figure out if a message was pinned or unpinned for logging purposes
-    if (ch.type !== "text") return;
+client.on("channelPinsUpdate", async (ch, date) => {
+    // we need to figure out if a message was pinned or unpinned
+    if (ch.type !== "GUILD_TEXT") return;
     const settings = guildSettings.get(ch.guild.id);
 
-    if (!settings.pinChannel && !settings.logChannel) return;
+    if (!settings.pinChannel) return;
 
-    if (!ch.permissionsFor(client.user).has("VIEW_CHANNEL")) return console.log(`message pins updated in ${ch.name}`);
-
-    const currentPins = await ch.messages.fetchPinned();
-    const previousPins = cachedPins.get(ch.id);
-    let size;
-
-    if (!previousPins) size = 0;
-    else size = previousPins.size;
-
-    if (currentPins.size > size) {
-        // message pinned
+    if (Date.now() - date.getTime() < 2000) {
+        // message pinned (probably)
+        const currentPins = await ch.messages.fetchPinned();
         const msg = currentPins.first();
-        if (settings.logChannel) {
-
-            const embed = new Discord.MessageEmbed;
-
-            embed.setAuthor(msg.author.tag, msg.author.displayAvatarURL({ format: "png" }));
-            embed.setDescription(`**Message sent by ${msg.author} pinned in ${ch}**\n${msg.content}`);
-            embed.setFooter(`ID: ${msg.id}`);
-            embed.setTimestamp();
-            embed.setColor(0x23D160);
-            if (msg.attachments.size !== 0) embed.setImage(msg.attachments.first().url);
-
-            client.channels.cache.get(settings.logChannel).send({ embed }).catch(() => { });
-        }
 
         if (settings.pinChannel && !settings.pins.includes(msg.id)) {
             const webhooks = await client.channels.cache.get(settings.pinChannel).fetchWebhooks().catch(() => { /* I'm cheating */ }); // throws an error when we don't have access
             if (!webhooks) return;
 
+            /** @type {Discord.Webhook} */
             let webhook;
 
             if (webhooks.size === 0) {
                 // try and make a new webhook
                 webhook = await client.channels.cache.get(settings.pinChannel).createWebhook("PinBot", client.user.avatarURL({ format: "png" }), "A webhook is required for impersonation");
             } else {
-                webhook = webhooks.array()[0]; // the webhook we use doesn't mater since we override the profile picture and name
+                webhook = webhooks.first(); // the webhook we use doesn't matter since we override the profile picture and name
             }
 
             const files = msg.attachments.size !== 0 ? [msg.attachments.first().url] : [];
-            webhook.send(msg.cleanContent.replace(/@/g, "@" + String.fromCharCode(8203)), { username: msg.author.username, avatarURL: msg.author.displayAvatarURL({ format: "png" }), files, embeds: msg.embeds, disableEveryone: true }); // impersonate the author
+            let content = undefined;
+            if (msg.cleanContent.length > 0) content = msg.cleanContent.replace(/@/g, "@" + String.fromCharCode(8203));
+            webhook.send({ content: content, username: msg.author.username, avatarURL: msg.author.displayAvatarURL({ format: "png" }), files: files, embeds: msg.embeds }); // impersonate the author
             settings.pins.push(msg.id);
             guildSettings.set(ch.guild.id, settings);
         }
-    } else if (currentPins.size < previousPins.size && settings.logChannel) {
-        // message unpinned
-        const embed = new Discord.MessageEmbed;
-
-        previousPins.forEach(pin => {
-            if (!currentPins.has(pin.id)) {
-                // found the removed pin
-                const msg = pin;
-
-                embed.setAuthor(msg.author.tag, msg.author.displayAvatarURL({ format: "png" }));
-                embed.setDescription(`**Message sent by ${msg.author} unpinned in ${ch}**\n${msg.content}`);
-                embed.setFooter(`ID: ${msg.id}`);
-                embed.setTimestamp();
-                embed.setColor(0xFF470F);
-                if (msg.attachments.size !== 0) embed.setImage(msg.attachments.first().url);
-
-                client.channels.cache.get(settings.logChannel).send({ embed }).catch(() => { });
-            }
-        });
     }
-    cachedPins.set(ch.id, currentPins);
 });
 
 client.on("messageReactionAdd", async (react, user) => {
@@ -164,7 +89,7 @@ client.on("messageReactionAdd", async (react, user) => {
         await user.fetch();
     }
 
-    if (react.message.channel.type !== "text" || user.id === react.message.author.id || react.emoji.name !== "📌") return;
+    if (react.message.channel.type !== "GUILD_TEXT" || user.id === react.message.author.id || react.emoji.name !== "📌") return;
 
     const settings = guildSettings.get(react.message.guild.id);
 
@@ -184,18 +109,20 @@ client.on("messageReactionAdd", async (react, user) => {
         // try and make a new webhook
         webhook = await pinChannel.createWebhook("PinBot", client.user.avatarURL({ format: "png" }), "A webhook is required for impersonation");
     } else {
-        webhook = webhooks.array()[0]; // the webhook we use doesn't mater since we override the profile picture and name
+        webhook = webhooks.first(); // the webhook we use doesn't mater since we override the profile picture and name
     }
 
     const msg = react.message;
     const files = msg.attachments.size !== 0 ? [msg.attachments.first().url] : [];
-    webhook.send(msg.cleanContent.replace(/@/g, "@" + String.fromCharCode(8203)), { username: msg.author.username, avatarURL: msg.author.displayAvatarURL({ format: "png" }), files, embeds: msg.embeds, disableEveryone: true }); // impersonate the author
+    let content = undefined;
+    if (msg.cleanContent.length > 0) content = msg.cleanContent.replace(/@/g, "@" + String.fromCharCode(8203));
+    webhook.send({ content: content, username: msg.author.username, avatarURL: msg.author.displayAvatarURL({ format: "png" }), files: files, embeds: msg.embeds }); // impersonate the author
     settings.pins.push(msg.id);
     guildSettings.set(msg.guild.id, settings);
 });
 
-client.on("message", async msg => {
-    if (msg.channel.type !== "text") return;
+client.on("messageCreate", async msg => {
+    if (msg.channel.type !== "GUILD_TEXT") return;
     if (!msg.content.startsWith(prefix)) return;
     if (!msg.channel.permissionsFor(client.user).has("SEND_MESSAGES")) return;
     if (msg.author.bot) return;
@@ -203,15 +130,14 @@ client.on("message", async msg => {
     const args = msg.content.split(" ").slice(1);
     const cmd = msg.content.slice(prefix.length).split(" ")[0];
 
+    if (!guildSettings.has(msg.guild.id)) guildSettings.set(msg.guild.id, defaultSettings);
     const settings = guildSettings.get(msg.guild.id);
 
     switch (cmd) {
         case "help":
             msg.channel.send(`Commands list: \`\`\`
-${prefix}set logs <channel>
 ${prefix}set pins <channel>
 ${prefix}set reacts <number of reactions required to auto-pin a message>
-${prefix}unset logs
 ${prefix}unset pins
 ${prefix}unset reacts
 ${prefix}help\`\`\``);
@@ -219,20 +145,13 @@ ${prefix}help\`\`\``);
         case "set":
             if (!msg.member.permissions.has("MANAGE_GUILD", true) && msg.author.id !== "221017760111656961") return msg.channel.send("You need to have Manage Server permissions to use this command!");
             switch (args[0]) {
-                case "logs":
-                    if (msg.mentions.channels.size !== 1) return msg.channel.send(`Usage: ${prefix}set logs <channel>`, { code: "" });
-                    if (msg.mentions.channels.first().guild.id !== msg.guild.id) return msg.channel.send("That channel isn't in this server!");
-                    if (msg.mentions.channels.first().type !== "text") return msg.channel.send("Not a text channel!");
-                    settings.logChannel = msg.mentions.channels.first().id;
-                    guildSettings.set(msg.guild.id, settings);
-                    return msg.channel.send("Successfully set log channel!");
                 case "pins":
                     if (msg.mentions.channels.size !== 1) return msg.channel.send(`Usage: ${prefix}set pins <channel>`, { code: "" });
                     if (msg.mentions.channels.first().guild.id !== msg.guild.id) return msg.channel.send("That channel isn't in this server!");
-                    if (msg.mentions.channels.first().type !== "text") return msg.channel.send("Not a text channel!");
+                    if (msg.mentions.channels.first().type !== "GUILD_TEXT") return msg.channel.send("Not a text channel!");
                     settings.pinChannel = msg.mentions.channels.first().id;
                     guildSettings.set(msg.guild.id, settings);
-                    return msg.channel.send("Successfully set pin channel! Make sure I have permission to manage webhooks for this channel!");
+                    return msg.channel.send("Successfully set pin channel! Make sure I have permission to manage webhooks!");
                 case "reacts":
                     if (args.length !== 2) return msg.channel.send(`Usage: ${prefix}set reacts <number of reactions required to auto-pin a message>`, { code: "" });
                     if (isNaN(parseInt(args[1]))) return msg.channel.send("Not a number!");
@@ -244,10 +163,6 @@ ${prefix}help\`\`\``);
         case "unset":
             if (!msg.member.permissions.has("MANAGE_GUILD", true) && msg.author.id !== "221017760111656961") return msg.channel.send("You need to have Manage Server permissions to use this command!");
             switch (args[0]) {
-                case "logs":
-                    settings.logChannel = defaultSettings.logChannel;
-                    guildSettings.set(msg.guild.id, settings);
-                    return msg.channel.send("Successfully unset log channel!");
                 case "pins":
                     settings.pinChannel = defaultSettings.pinChannel;
                     guildSettings.set(msg.guild.id, settings);
