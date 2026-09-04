139f5a92290f25eb9cc48eb46fbd0a68d8c4cad4
IntriguingTiles <jnickw@outlook.com>
2026-09-03 19:17:03 -0600

    Status tweak

--- src/bot.ts
+++ src/bot.ts
@@ -25,7 +25,7 @@ const client = new Client({
         activities: [
             {
                 type: ActivityType.Watching,
-                name: "📌"
+                name: "📌 /config"
             }
         ]
     }
